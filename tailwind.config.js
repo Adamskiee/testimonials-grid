e371272a12b39c235b472ca8a466d98ab61f6be9
Adamskiee <tosinomatthew@gmail.com>
2025-08-07 15:45:10 +0800

    feat: enhance testimonials section with responsive design and improved styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,26 @@ module.exports = {
   content: ["./build/*.html"],
   theme: {
     extend: {
+      screens: {
+        "min-desktop": "1440px"
+      },
       fontFamily: {
         barlow: ["Barlow Semi Condensed","sans-serif"],
+      },
+      colors: {
+        "purple-50": "var(--purple-50)",
+        "purple-300": "var(--purple-300)",
+        "purple-500": "var(--purple-500)",
+        "white": "var(--white)",
+        "grey-100": "var(--grey-100)",
+        "grey-200": "var(--grey-200)",
+        "grey-400": "var(--grey-400)",
+        "grey-500": "var(--grey-500)",
+        "darkblue": "var(--darkblue)",
+        "black": "var(--black)",
+      },
+      boxShadow: {
+        "3xl": "var(--shadow-3xl)",
       }
     },
   },
